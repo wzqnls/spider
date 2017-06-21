fc874c06ed38f73f38f573e07de151dd81e79205
lee <wzqnls@gmail.com>
2017-06-21 16:42:02 +0800

    selenium模拟知乎,微博登录

---
+++ ArticleSpider/tools/selenium_spider.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17-6-21 下午2:18
+# @Author  : Lee
+# @File    : selenium_spider.py
+
+from selenium import webdriver
+from scrapy.selector import Selector
+
+browser = webdriver.Chrome(executable_path="/home/lee/Downloads/chromedriver")
+
+browser.get("https://www.zhihu.com/#signin")
+
+browser.find_element_by_css_selector(".view-signin input[name='account']").send_keys('13735846612')
+browser.find_element_by_css_selector(".view-signin input[name='password']").send_keys('codewithpython666')
+
+browser.find_element_by_css_selector(".view-signin button.sign-button").click()
+
+# t_selector = Selector(text=browser.page_source)
+# test = t_selector.css(".tm-promo-price .tm-price::text").extract()
+# print(test)
+
+# 微博模拟登录
+browser.get("https://www.weibo.com")
+
+import time
+time.sleep(10)
+# browser.find_element_by_css_selector("#loginname").send_keys("")
+# browser.find_element_by_css_selector(".info_list.password input[node-type='password']").send_keys("")
+# browser.find_element_by_css_selector(".info_list.login_btn a[node-type='submitBtn']").click()
+
+
+
+# browser.quit()
